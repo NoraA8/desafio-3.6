275f91da001b32121db7dfa017ff7443f2bfa651
NoraA8 <zaske8@gmail.com>
2022-12-13 20:25:41 -0300

    clear

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,56 +1,62 @@
-const amount = document.querySelector("#amount")
-const currency = document.querySelector("#currency")
-const dolar = document.querySelector("#dolar")
-const euro = document.querySelector("#euro")
-const btnSearch = document.querySelector("#btnSearch")
-const results = document.querySelector("#results")
-const ctx = document.querySelector("#myChart")
+const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
+const dolar = document.querySelector("#dolar");
+const euro = document.querySelector("#euro");
+const btnSearch = document.querySelector("#btnSearch");
+const results = document.querySelector("#results");
+const ctx = document.querySelector("#myChart");
 
 const getConverter = async () => {
-    try {
-      const response = await fetch("https://mindicador.cl/api")
-      const data = await response.json()
-      if(!response.ok) throw "Fallo la solicitud"
-      results.innerHTML = `Resultado: ${parseFloat(amount.value) / data[currency.value].valor}`
-    } catch (error) {
-      results.innerHTML = error
-    } 
+  try {
+    const response = await fetch("https://mindicador.cl/api");
+    const data = await response.json();
+    if (!response.ok) throw "Fallo la solicitud";
+    results.innerHTML = `Resultado: ${
+      parseFloat(amount.value) / data[currency.value].valor
+    }`;
+  } catch (error) {
+    results.innerHTML = error;
+  }
 };
 
-let graphic
+let graphic;
 
-const redenderChart = async(currency) => {
-  console.log(currency)
-  const res = await fetch(`https://mindicador.cl/api/${currency}`)
-  const data = await res.json()
-  const arrayChart = data.serie.slice(0, 10).reverse()
+const redenderChart = async (currency) => {
+  console.log(currency);
+  const res = await fetch(`https://mindicador.cl/api/${currency}`);
+  const data = await res.json();
+  const arrayChart = data.serie.slice(0, 10).reverse();
 
-  const labels = arrayChart.map(item => item.fecha.split("T")[0].split("-").reverse().join("-"))
-  const dataCurrency = arrayChart.map(item => item.valor)
+  const labels = arrayChart.map((item) =>
+    item.fecha.split("T")[0].split("-").reverse().join("-")
+  );
+  const dataCurrency = arrayChart.map((item) => item.valor);
 
   if (graphic) {
-    graphic.destroy()
+    graphic.destroy();
   }
 
-  graphic = new Chart (ctx, {
-    type: 'line',
+  graphic = new Chart(ctx, {
+    type: "line",
     data: {
       labels: labels,
-      datasets: [{
-        label: 'Ultimos 10 dias',
-        data: dataCurrency,
-        borderWidth: 1
-      }]
+      datasets: [
+        {
+          label: "Ultimos 10 dias",
+          data: dataCurrency,
+          borderWidth: 1,
+        },
+      ],
     },
-  })
-}
+  });
+};
 
 btnSearch.addEventListener("click", () => {
-  const inputAmount = amount.value
+  const inputAmount = amount.value;
   if (!inputAmount) {
     alert("Faltan campos por llenar");
     return;
   }
   getConverter();
   redenderChart(currency.value);
-})
+});
